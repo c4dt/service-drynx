fd8756d4fb90579756839ebf8643fd8bb6a9f0b8
tharvik <tharvik@users.noreply.github.com>
2020-01-25 12:14:06 +0100

    webapp: move results to plotter

--- webapp/src/app/results-plotter/results-plotter.component.ts
+++ webapp/src/app/results-plotter/results-plotter.component.ts
@@ -1,53 +1,50 @@
 import { List, Range } from 'immutable'
 
 import { Component, Input, OnChanges } from '@angular/core'
+import { Result, ResultType } from '../columns'
 
-// A very simple plotter class that takes into account the scaling happening
-// in the drynx-demonstrator.
-// It plots 100 points from x_real = 0 to x_real = xmax * factors[0]
 @Component({
   selector: 'app-results-plotter',
   templateUrl: './results-plotter.component.html'
 })
 export class ResultsPlotterComponent implements OnChanges {
-  // factors to calculate
-  //   x_real = x_db * factor_0
-  // and
-  //   y_real = y_db * factor_1
-  @Input() factors: number[] = [1, 1]
-  // betas from the linear regression: y_db = beta_0 + beta_1 * x_db
-  @Input() betas: List<number> | undefined
+  @Input() public results: List<[ResultType, Result]> | null | undefined
+
+  public graph: Array<{ x: number[], y: number[], type: string, mode: string }> | undefined
 
   private readonly range = Range(0, 10)
 
-  public graph: { data: Array<{ x: number[], y: number[], type: string, mode: string }> } | undefined
-
   ngOnChanges (_: any): void {
-    if (this.factors === undefined || this.betas === undefined) {
+    if (this.results === undefined || this.results === null) {
       return
     }
 
-    if (this.factors.length !== this.betas.size ||
-      this.factors.length !== 2) {
+    if (this.results.size === 1) {
+      return // do not update graph
+    }
+
+    if (this.results.size !== 2) {
       throw new Error("don't know how to do those yet")
     }
 
-    const rawA = this.betas.get(1)
-    const rawB = this.betas.get(0)
-    if (rawA === undefined || rawB === undefined) {
+    const rawA = this.results.get(1)
+    const rawB = this.results.get(0)
+    if (rawA === undefined || rawA[0] !== 'number' ||
+      rawB === undefined || rawB[0] !== 'number') {
       throw new Error()
     }
 
-    const xPoints = this.range.toArray()
-    const yPoints = this.range.map(x => rawA * x + rawB).toArray()
+    const a = rawA[1] as number
+    const b = rawB[1] as number
 
-    this.graph = {
-      data: [{
-        x: xPoints,
-        y: yPoints,
-        type: 'scatter',
-        mode: 'lines'
-      }]
-    }
+    const xPoints = this.range.toArray()
+    const yPoints = this.range.map(x => a * x + b).toArray()
+
+    this.graph = [{
+      x: xPoints,
+      y: yPoints,
+      type: 'scatter',
+      mode: 'lines'
+    }]
   }
 }
